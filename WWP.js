b74509b8685cfb8cc0443a981504b716cad51e38
koganm1 <koganm1@mcmaster.ca>
2023-04-06 13:50:13 -0400

    added qr-code functionality (and a bunch of other files that can probably be deleted)

--- WWP.js
+++ WWP.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 export default function Wordle() {
+
   const [word, setWord] = useState("");
   const [guess, setGuess] = useState("");
   const [wrongGuesses, setWrongGuesses] = useState(0);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [gameState, setGameState] = useState("ongoing");
+
+
 
   const checkGuess = () => {
     if (word === guess) {
@@ -85,62 +88,61 @@ export default function Wordle() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#f7f7f7",
-    },
-    title: {
-      fontSize: 30,
-      marginBottom: 20,
-      fontWeight: "bold",
-      color: "#444444",
-    },
-    word: {
-      fontSize: 20,
-      marginBottom: 20,
-      fontWeight: "bold",
-      color: "#444444",
-    },
-    inputContainer: {
-      flexDirection: "row",
-      marginBottom: 20,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: "#cccccc",
-      padding: 10,
-      flex: 1,
-      marginRight: 10,
-      borderRadius: 5,
-      color: "#444444",
-    },
-    resultContainer: {
-      alignItems: "center",
-    },
-    resultText: {
-      fontSize: 30,
-      marginBottom: 20,
-      fontWeight: "bold",
-      color: "#444444",
-    },
-    button: {
-      backgroundColor: "#0077cc",
-      padding: 10,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: "white",
-      fontWeight: "bold",
-    },
-    wrongGuess: {
-      color: "#ff3333",
-      fontWeight: "bold",
-    },
-    correctGuess: {
-      color: "#33cc33",
-      fontWeight: "bold",
-    },
-  });
-  
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f7f7f7",
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+    fontWeight: "bold",
+    color: "#444444",
+  },
+  word: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: "bold",
+    color: "#444444",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    padding: 10,
+    flex: 1,
+    marginRight: 10,
+    borderRadius: 5,
+    color: "#444444",
+  },
+  resultContainer: {
+    alignItems: "center",
+  },
+  resultText: {
+    fontSize: 30,
+    marginBottom: 20,
+    fontWeight: "bold",
+    color: "#444444",
+  },
+  button: {
+    backgroundColor: "#0077cc",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  wrongGuess: {
+    color: "#ff3333",
+    fontWeight: "bold",
+  },
+  correctGuess: {
+    color: "#33cc33",
+    fontWeight: "bold",
+  },
+});
